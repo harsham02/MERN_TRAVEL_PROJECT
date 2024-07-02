9b4ff21bcaba77f002eaf996afdcd97909f89268
harsham02 <harshavardhan15015@gmail.com>
2024-07-02 23:21:30 +0530

    second-commit

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended : true}));
 
 app.use(cors({
-    origin: 'http://localhost:5173',  
+    origin: 'https://mern-travel-project.vercel.app',  
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type'],
     credentials: true
